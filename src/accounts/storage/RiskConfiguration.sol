749e9350f65058010ce14a7af61eb5100492926b
Artur Begyan <arturbegyan98@gmail.com>
2023-02-27 21:49:33 +0000

    risk configuration setting

--- src/accounts/storage/RiskConfiguration.sol
+++ src/accounts/storage/RiskConfiguration.sol
@@ -8,19 +8,53 @@ library RiskConfiguration {
     struct Data {
         // todo: can we pack parameters in a more gas/storage efficient way?
         /**
-         * @dev Risk Parameter Mapping: productId (e.g. Dated IRS) => marketId (e.g. aUSDC lend) => riskParameter (e.g. 0.1)
+         * @dev Id of the product for which we store risk configurations
          */
-        mapping(uint128 => mapping(uint128 => int256)) riskParameters;
+        uint128 productId;
         /**
-         * @dev Initial Margin (IM) Requirement Multiplier Mapping: productId (e.g. Dated IRS) => marketId (e.g. aUSDC lend) => imMultiplier (e.g. 1.5)
+         * @dev Id of the market for which we store risk configurations
+         */
+        uint128 marketId;
+        /**
+         * @dev Risk Parameters are multiplied by notional exposures to derived shocked cashflow calculations
+         */
+        int256 riskParameter;
+        /**
          * @dev IM Multipliers are used to introduce a buffer between the liquidation and initial margin requirements
          * where IM = imMultiplier * LM
          */
-        mapping(uint128 => mapping(uint128 => int256)) imMultipliers;
+        int256 imMultiplier;
         /**
-         * @dev Liquidator Reward Parameter Mapping: productId (e.g. Dated IRS) => marketId (e.g. aUSDC lend) => riskParameter (e.g. 0.1)
          * @dev Liquidator reward parameters are multiplied by the im delta caused by the liquidation to get the liquidator reward amount
          */
-        mapping(uint128 => mapping(uint128 => int256)) liquidatorRewardParameters;
+        int256 liquidatorRewardParameter;
+    }
+
+    /**
+     * @dev Loads the RiskConfiguration object for the given collateral type.
+     * @param productId Id of the product (e.g. IRS) for which we want to query the risk configuration
+     * @param marketId Id of the market (e.g. aUSDC lend) for which we want to query the risk configuration
+     * @return riskConfiguration The RiskConfiguration object.
+     */
+    // todo: stopped here
+    function load(uint128 productId, uint128 marketId) internal pure returns (Data storage riskConfiguration) {
+        bytes32 s = keccak256(abi.encode("xyz.voltz.RiskConfiguration", productId, marketId));
+        assembly {
+            riskConfiguration.slot := s
+        }
+    }
+
+    /**
+     * @dev Sets the risk configuration for a given productId & marketId pair
+     * @param config The RiskConfiguration object with all the risk parameters
+     */
+    function set(Data memory config) internal {
+        Data storage storedConfig = load(config.productId, config.marketId);
+
+        storedConfig.productId = config.productId;
+        storedConfig.marketId = config.marketId;
+        storedConfig.riskParameter = config.riskParameter;
+        storedConfig.imMultiplier = config.imMultiplier;
+        storedConfig.liquidatorRewardParameter = config.liquidatorRewardParameter;
     }
 }
