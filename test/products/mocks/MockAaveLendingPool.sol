5391a2f8c5d3142a22133a2011a4c1801f25be4e
CyclopsRex <cyclopsrex@voltz.xyz>
2023-03-02 09:08:26 +0000

    Update MockAaveLendingPool

--- test/products/mocks/MockAaveLendingPool.sol
+++ test/products/mocks/MockAaveLendingPool.sol
@@ -3,16 +3,16 @@
 pragma solidity =0.8.17;
 import "src/products/externalInterfaces/IAaveV3LendingPool.sol";
 import "oz/interfaces/IERC20.sol";
-import { UD60x18 } from "@prb/math/UD60x18.sol";
+import { UD60x18, ud } from "@prb/math/UD60x18.sol";
 
 /// @notice This Mock Aave pool can be used in 3 ways
 /// - change the rate to a fixed value (`setReserveNormalizedIncome`)
 /// - configure the rate to alter over time (`setFactorPerSecondInRay`) for more dynamic testing
 contract MockAaveLendingPool is IAaveV3LendingPool {
-    mapping(address => uint256) internal reserveNormalizedIncome;
+    mapping(address => UD60x18) internal reserveNormalizedIncome;
     // mapping(IERC20 => uint256) internal reserveNormalizedVariableDebt;
-    mapping(address => uint256) internal startTime;
-    // mapping(IERC20 => uint256) internal factorPerSecondInRay; // E.g. 1000000001000000000000000000 for 0.0000001% per second = ~3.2% APY
+    mapping(address => uint40) internal startTime;
+    mapping(address => UD60x18) internal factorPerSecond; // E.g. 1000000001000000000 for 0.0000001% per second = ~3.2% APY
 
     function getReserveNormalizedIncome(address _underlyingAsset)
         public
@@ -20,67 +20,30 @@ contract MockAaveLendingPool is IAaveV3LendingPool {
         override
         returns (uint256)
     {
-        // uint256 factorPerSecond = factorPerSecondInRay[_underlyingAsset];
-        // if (factorPerSecond > 0) {
-        //     uint256 secondsSinceNormalizedIncomeSet = block.timestamp -
-        //         startTime[_underlyingAsset];
-        //     return
-        //         PRBMathUD60x18.mul(
-        //             reserveNormalizedIncome[_underlyingAsset],
-        //             PRBMathUD60x18.pow(
-        //                 factorPerSecond,
-        //                 secondsSinceNormalizedIncomeSet
-        //             )
-        //         );
-        // } else {
-            return reserveNormalizedIncome[_underlyingAsset];
-        // }
-    }
+        UD60x18 factor = factorPerSecond[_underlyingAsset];
+        UD60x18 currentIndex = reserveNormalizedIncome[_underlyingAsset];
+        if (factor.unwrap() > 0) {
+            uint256 secondsSinceNormalizedIncomeSet = block.timestamp -
+                startTime[_underlyingAsset];
+            currentIndex = reserveNormalizedIncome[_underlyingAsset].mul(factor.powu(secondsSinceNormalizedIncomeSet));
+        }
 
-//     function getReserveNormalizedVariableDebt(IERC20 _underlyingAsset)
-//         public
-//         view
-//         returns (uint256)
-//     {
-//         UD60x18 factorPerSecond = factorPerSecondInRay[_underlyingAsset];
-//         if (factorPerSecond > 0) {
-//             uint256 secondsSinceNormalizedVariableDebtSet = block.timestamp -
-//                 startTime[_underlyingAsset];
-//             return
-//                 PRBMathUD60x18.mul(
-//                     reserveNormalizedVariableDebt[_underlyingAsset],
-//                     PRBMathUD60x18.pow(
-//                         factorPerSecond,
-//                         secondsSinceNormalizedVariableDebtSet
-//                     )
-//                 );
-//         } else {
-//             return reserveNormalizedVariableDebt[_underlyingAsset];
-//         }
-//     }
+        // Convert from UD60x18 to Aave's "Ray" (decmimal scaled by 10^27) to confrom to Aave interface
+        return currentIndex.intoUint256() * 1e9;
+    }
 
     function setReserveNormalizedIncome(
         IERC20 _underlyingAsset,
-        uint256 _reserveNormalizedIncome
+        UD60x18 _reserveNormalizedIncomeInWeiNotRay
     ) public {
-        reserveNormalizedIncome[address(_underlyingAsset)] = _reserveNormalizedIncome;
-        startTime[address(_underlyingAsset)] = block.timestamp;
+        reserveNormalizedIncome[address(_underlyingAsset)] = _reserveNormalizedIncomeInWeiNotRay;
+        startTime[address(_underlyingAsset)] = uint40(block.timestamp);
     }
 
-//     function setReserveNormalizedVariableDebt(
-//         IERC20 _underlyingAsset,
-//         uint256 _reserveNormalizedVariableDebt
-//     ) public {
-//         reserveNormalizedVariableDebt[
-//             _underlyingAsset
-//         ] = _reserveNormalizedVariableDebt;
-//         startTime[_underlyingAsset] = block.timestamp;
-//     }
-
-//     function setFactorPerSecondInRay(
-//         IERC20 _underlyingAsset,
-//         uint256 _factorPerSecondInRay
-//     ) public {
-//         factorPerSecondInRay[_underlyingAsset] = _factorPerSecondInRay;
-//     }
+    function setFactorPerSecondInRay(
+        IERC20 _underlyingAsset,
+        UD60x18 _factorPerSecond
+    ) public {
+        factorPerSecond[address(_underlyingAsset)] = _factorPerSecond;
+    }
 }
