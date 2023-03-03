44ac900df498f46f373f03b010be6e9d4ae02644
Artur Begyan <arturbegyan98@gmail.com>
2023-03-03 00:10:00 +0000

    pool configuration module skeleton

--- src/products/dated-irs/modules/PoolConfigurationModule.sol
+++ src/products/dated-irs/modules/PoolConfigurationModule.sol
@@ -1,2 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
+
+import "../interfaces/IPoolConfigurationModule.sol";
+import "../storage/PoolConfiguration.sol";
+import "../../../utils/storage/OwnableStorage.sol";
+
+/**
+ * @title Module for configuring the pool linked to the dated irs product
+ * @dev See IPoolConfigurationModule.
+ */
+contract PoolConfigurationModule is IPoolConfigurationModule {
+    using PoolConfiguration for PoolConfiguration.Data;
+
+    /**
+     * @inheritdoc IPoolConfigurationModule
+     */
+    function configurePool(PoolConfiguration.Data memory config) external { }
+
+    /**
+     * @inheritdoc IPoolConfigurationModule
+     */
+    function getPoolConfiguration() external view returns (PoolConfiguration.Data memory config) { }
+}
