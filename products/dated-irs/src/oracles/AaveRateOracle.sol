481ac01d99a43c3c99f8d173723e7e61a11fade4
Artur Begyan <arturbegyan98@gmail.com>
2023-06-27 14:47:41 +0100

    feat: resolve aave rate oracle todos

--- products/dated-irs/src/oracles/AaveRateOracle.sol
+++ products/dated-irs/src/oracles/AaveRateOracle.sol
@@ -10,12 +10,13 @@ pragma solidity >=0.8.19;
 import "../interfaces/IRateOracle.sol";
 import "../interfaces/external/IAaveV3LendingPool.sol";
 import "@voltz-protocol/util-contracts/src/helpers/Time.sol";
-// import "../rate_oracles/CompoundingRateOracle.sol";
 import { UD60x18, ud, unwrap } from "@prb/math/UD60x18.sol";
 
 contract AaveRateOracle is IRateOracle {
     IAaveV3LendingPool public aaveLendingPool;
     address public immutable underlying;
+
+    error AavePoolGetReserveNormalizedIncomeReturnedZero();
 
     constructor(IAaveV3LendingPool _aaveLendingPool, address _underlying) {
         require(address(_aaveLendingPool) != address(0), "aave pool must exist");
@@ -27,26 +28,26 @@ contract AaveRateOracle is IRateOracle {
     /// @inheritdoc IRateOracle
     function getLastUpdatedIndex() public view override returns (uint32 timestamp, UD60x18 liquidityIndex) {
         uint256 liquidityIndexInRay = aaveLendingPool.getReserveNormalizedIncome(underlying);
-        // note: Should we delete this commented code?
-        ///
-        // if (liquidityIndex == 0) {
-        //     revert CustomErrors.AavePoolGetReserveNormalizedIncomeReturnedZero();
-        // }
 
-        // Convert index from Aave's "ray" (decimal scaled by 10^27) to UD60x18 (decimal scaled by 10^18)
-        return (Time.blockTimestampTruncated(), ud(liquidityIndexInRay / 1e9));
+         if (liquidityIndexInRay == 0) {
+             revert AavePoolGetReserveNormalizedIncomeReturnedZero();
+         }
+
+        liquidityIndex = ud(liquidityIndexInRay / 1e9);
+
+        return (Time.blockTimestampTruncated(), liquidityIndex);
     }
 
     /// @inheritdoc IRateOracle
     function getCurrentIndex() external view override returns (UD60x18 liquidityIndex) {
         uint256 liquidityIndexInRay = aaveLendingPool.getReserveNormalizedIncome(underlying);
-        // note: Should we delete this commented code?
-        // if (liquidityIndex == 0) {
-        //     revert CustomErrors.AavePoolGetReserveNormalizedIncomeReturnedZero();
-        // }
 
-        // Convert index from Aave's "ray" (decimal scaled by 10^27) to UD60x18 (decimal scaled by 10^18)
-        return ud(liquidityIndexInRay / 1e9);
+        if (liquidityIndexInRay == 0) {
+            revert AavePoolGetReserveNormalizedIncomeReturnedZero();
+        }
+
+        liquidityIndex = ud(liquidityIndexInRay / 1e9);
+        return liquidityIndex;
     }
 
     /// @inheritdoc IRateOracle
