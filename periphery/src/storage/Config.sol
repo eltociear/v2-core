98cd985a78b050c60540d281a89d7a88a042beb1
Artur Begyan <arturbegyan98@gmail.com>
2023-06-28 21:04:25 +0100

    feat: remove reference to access pass in periphery

--- periphery/src/storage/Config.sol
+++ periphery/src/storage/Config.sol
@@ -12,8 +12,6 @@ library Config {
         address VOLTZ_V2_CORE_PROXY;
         address VOLTZ_V2_DATED_IRS_PROXY;
         address VOLTZ_V2_DATED_IRS_VAMM_PROXY;
-        // todo: note, transfer is no longer necessary, consider removing VOLTZ_V2_ACCOUNT_NFT_PROXY from the config
-        address VOLTZ_V2_ACCOUNT_NFT_PROXY;
     }
 
     /**
@@ -26,7 +24,6 @@ library Config {
         storedConfig.VOLTZ_V2_CORE_PROXY = config.VOLTZ_V2_CORE_PROXY;
         storedConfig.VOLTZ_V2_DATED_IRS_PROXY = config.VOLTZ_V2_DATED_IRS_PROXY;
         storedConfig.VOLTZ_V2_DATED_IRS_VAMM_PROXY = config.VOLTZ_V2_DATED_IRS_VAMM_PROXY;
-        storedConfig.VOLTZ_V2_ACCOUNT_NFT_PROXY = config.VOLTZ_V2_ACCOUNT_NFT_PROXY;
     }
 
     /**
